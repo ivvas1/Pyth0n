ddc4b5f4b77fbd54c503a3a01634a05a98089c85
Ivan Vasilev <112619137+ivvas1@users.noreply.github.com>
2023-05-22 13:01:36 +0300

    Create caesar_cipher.py

---
+++ 03.1.FunctionsStringsIO/caesar_cipher/caesar_cipher.py
@@ -0,0 +1,23 @@
+def caesar_encrypt(message: str, n: int) -> str:
+    """Encrypt message using caesar cipher
+
+    :param message: message to encrypt
+    :param n: shift
+    :return: encrypted message
+    """
+    ans = ""
+    for i in message:
+        k = ord(i)
+        if (k >= 65 and k <= 90) or (k >= 97 and k <= 122):
+            if k >= 65 and k <= 90:
+                k += n % 26
+                if k > 90:
+                    k -= 26
+            else:
+                k += n % 26
+                if k > 122:
+                    k -= 26
+        ans += chr(k)
+    return ans
+
+
